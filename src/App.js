4c771ffeeeaefb7303bd99f91297ff7be37bda76
mingyeongmo <mingyeongmp@naver.com>
2021-07-20 15:19:12 +0900

    useReducer managing the input state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Counter from './Components/Counter';
+import React from 'react';
+import Info from './Components/Info';
 
 function App() {
   return (
     <>
-      <Counter />
+      <Info />
     </>
   );
 }
